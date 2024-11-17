d5f97b054880216bd8abd098f96c1cef09c83e47
Serhiy <serhii.zabachynskyi-ki241@nung.edu.ua>
2024-11-17 20:39:37 +0200

    Виконав заміну від'ємних елементів що знаходяться на парних місцях середнім значенням всіх від'ємних елементів масиву, а елементи, що знаходяться на непарних місцях, замінити середнім значенням всіх додатних елементів масиву.

--- laba5komanda.cpp
+++ laba5komanda.cpp
@@ -36,6 +36,40 @@ int main() {
             cout << arr[i] << " ";
         }
     }
+
+    cout << endl;
+
+    int sumNegative = 0, countNegative = 0;
+    int sumPositive = 0, countPositive = 0;
+
+    for (int i = 0; i < 14; ++i) {
+        if (arr[i] < 0) {
+            sumNegative += arr[i];
+            countNegative++;
+        }
+        else if (arr[i] > 0) {
+            sumPositive += arr[i];
+            countPositive++;
+        }
+    }
+
+    int avgNegative = (countNegative > 0) ? sumNegative / countNegative : 0;
+    int avgPositive = (countPositive > 0) ? sumPositive / countPositive : 0;
+    cout << "Negative average: " << avgNegative << endl << "Positive average: " << avgPositive << endl;
+
+    for (int i = 0; i < 14; ++i) {
+        if (i % 2 == 0 && arr[i] < 0) {
+            arr[i] = avgNegative;
+        }
+        else if (i % 2 != 0) {
+            arr[i] = avgPositive;
+        }
+    }
+
+    cout << "Result array: ";
+    for (int i = 0; i < 14; ++i) {
+        cout << arr[i] << " ";
+    }
     cout << endl;
 
 }
